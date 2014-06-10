230ce37598e73d16059ecf30596b2e78b9cd75c1
Thiago Cardoso <thiagoncc@gmail.com>
2014-06-10 17:13:27 -0300

    Added attachment to SendRawEmail test

--- ses_test.go
+++ ses_test.go
@@ -1,6 +1,7 @@
 package ses
 
 import (
+    "encoding/base64"
 	"flag"
 	"fmt"
 	"testing"
@@ -37,6 +38,7 @@ func TestSendEmailHTML(t *testing.T) {
 
 func TestSendRawEmail(t *testing.T) {
 	checkFlags(t)
+    attachment := base64.StdEncoding.EncodeToString([]byte(textBody))
 	raw := `To: %s
 From: %s
 Subject: amzses raw test
@@ -47,9 +49,19 @@ MIME-Version: 1.0
 Content-Type: text/plain;
 
 %s
---_003_97DCB304C5294779BEBCFC8357FCC4D2`
 
-	_, err := EnvConfig.SendRawEmail([]byte(fmt.Sprintf(raw, to, from, textBody)))
+--_003_97DCB304C5294779BEBCFC8357FCC4D2
+Content-Type: text/plain; name="test.txt"
+Content-Description: test.txt
+Content-Disposition: attachment; filename="test.txt"; size=%d;
+Content-Transfer-Encoding: base64
+
+%s
+
+--_003_97DCB304C5294779BEBCFC8357FCC4D2
+`
+
+	_, err := EnvConfig.SendRawEmail([]byte(fmt.Sprintf(raw, to, from, textBody, len(attachment), attachment)))
 	if err != nil {
 		t.Fatal(err)
 	}
